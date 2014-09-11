ad2851d425252db036cea4357e32c5b854394c5e
Dmitriy Voronin <b0p0n92@gmail.com>
2014-09-11 15:12:18 +0400

    added calculation and output current montha

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,6 @@
 function onOpen() {
 	var moment = require('alloy/moment');
-	moment.lang('ru');	
+	//moment.lang('ru');	
     var date = moment().format('MMMM');
     var ruMonth;
     if (date == "January ") {
@@ -41,9 +41,101 @@ function onOpen() {
     }
     $.MonthLabel.text = ruMonth;
     $.YearLabel.text = moment().format('YYYY');
+    fillMonth();
+    
+}
+
+function fillMonth(dayCount, dayCountPrev) {
+	dayCount = 30;
+	dayCountPrev = 31;
+	var moment = require('alloy/moment');
+	//moment.lang('ru');	
+    var number = moment().format('DD');
+    var dayName = moment().format('dddd');
+    var dayNumber;
+    if(dayName == "Monday") {
+    	dayNumber = 0;
+    }
+    if(dayName == "Tuesday") {
+    	dayNumber = 1;
+    }
+    if(dayName == "Wednesday") {
+    	dayNumber = 2;
+    }
+    if(dayName == "Thursday") {
+    	dayNumber = 3;
+    }
+    if(dayName == "Friday") {
+    	dayNumber = 4;
+    }
+    if(dayName == "Saturday") {
+    	dayNumber = 5;
+    }
+    if(dayName == "Sunday") {
+    	dayNumber = 6;
+    }
+    var currentWeek =[];
+    currentWeek[dayNumber] = parseInt(number);
+    var dayNumberOfFirst = dayNumber;
+    for (var i= 1; i < 31; i++) {
+    	if(parseInt(number) - parseInt(i) == 0) {
+    		break;
+    	}    	
+    	dayNumberOfFirst--;
+    	if(dayNumberOfFirst == -1) {
+    		dayNumberOfFirst = 6;
+    	}
+    }
+    month = [];   
+    for(var i = 0; i < 42; i++) {
+    	if (i < dayNumberOfFirst) {    		
+    		month[i] = parseInt(dayCountPrev) - parseInt(dayNumberOfFirst) + parseInt(1) + parseInt(i);
+    	}
+    	if (i == dayNumberOfFirst) {
+    		month[i] = 1;
+    	}
+    	if ((i > dayNumberOfFirst) && i < (parseInt(dayCount) + parseInt(dayNumberOfFirst))) {
+    		month[i] = parseInt(month[i - 1]) + parseInt(1);
+    	}
+    	if (i > (parseInt(dayCount) + parseInt(dayNumberOfFirst) - parseInt(1))) {      		
+    		month[i] = parseInt(i) - parseInt(dayCount) - parseInt(dayNumberOfFirst) + parseInt(1);
+    	}
+    }
+    var weekView;
+    for (var i = 0; i < 42; i++) {
+    	if(i > -1 && i < 7){
+    		weekView = $.week1;
+    	}
+    	if(i > 6 && i < 14){
+    		weekView = $.week2;
+    	}
+    	if(i > 13 && i < 21){
+    		weekView = $.week3;
+    	}
+    	if(i > 20 && i < 28){
+    		weekView = $.week4;
+    	}
+    	if(i > 27 && i < 35){
+    		weekView = $.week5;
+    	}
+    	if(i > 34 && i < 42){
+    		weekView = $.week6;
+    	}    	
+    	var lbl = Ti.UI.createLabel({
+  			text: month[i],
+  			left: 5,
+  			right: 5,
+  			height: 21,
+  			width: 19
+		});
+    	weekView.add(lbl);
+    }
+    
+    
 }
 
 function onClickMonthBack() {	
+	debugger
 	if($.MonthLabel.text == 'Февраль') {
 		$.MonthLabel.text = 'Январь';	
 	}
